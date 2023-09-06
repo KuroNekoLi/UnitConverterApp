2cb5d21f2ccc7d162aad4cbfd16e76f1afe8a9c4
KuroNekoLi <alice.margatroid.love@gmail.com>
2023-09-06 11:22:27 +0800

    Create DropdownMenu

---
+++ app/src/main/java/com/example/unitconverterapp/BaseScreen.kt
@@ -0,0 +1,30 @@
+package com.example.unitconverterapp
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun BaseScreen(
+    modifier: Modifier = Modifier, //將預設的modifier設為第一個參數
+    converterViewModel: ConverterViewModel = viewModel()
+){
+    val list = converterViewModel.getConversions()
+    Column(modifier = Modifier.padding(30.dp)) {
+        TopScreen(list)
+        Spacer(modifier = Modifier.height(20.dp))
+        HistoryScreen()
+    }
+}
+
+@Preview
+@Composable
+fun BaseScreenPreview() {
+    BaseScreen(Modifier, viewModel())
+}
---
+++ app/src/main/java/com/example/unitconverterapp/HistoryScreen.kt
@@ -0,0 +1,6 @@
+package com.example.unitconverterapp
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HistoryScreen(){}
