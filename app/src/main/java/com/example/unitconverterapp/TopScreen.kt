e94f3936b5e3a13022a7ec04b7e033f7f73a5c76
KuroNekoLi <alice.margatroid.love@gmail.com>
2023-09-06 18:06:48 +0800

    增加result

--- app/src/main/java/com/example/unitconverterapp/TopScreen.kt
+++ app/src/main/java/com/example/unitconverterapp/TopScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 //使用者剛進去app時不顯示input field 與 button
 //只有當使用者選擇時才顯示
@@ -19,6 +21,9 @@ fun TopScreen(list: List<Conversion>) {
     val inputText = remember {
         mutableStateOf("")
     }
+    val typedValue = remember {
+        mutableStateOf("0.0")
+    }
 
 //    ConversionMenu(list = list, convert = {}) //lambda在最後可以使用下面寫法
     ConversionMenu(list = list) {
@@ -29,6 +34,24 @@ fun TopScreen(list: List<Conversion>) {
     selectedConversion.value?.let {
         InputBlock(conversion = it, inputText = inputText,modifier = Modifier){ input->
             Log.i("LinLi", "User typed $input");
+            //將從lambda得到的input assign
+            typedValue.value = input
         }
+    }
+
+    if (typedValue.value != "0.0"){
+        //將使用者的輸入轉成double
+        val input = typedValue.value.toDouble()
+        val multiply = selectedConversion.value!!.multiplyBy
+        val result = input*multiply //可能是一個很多位小數
+
+        //以下是將小數擷取四位的方法
+        val df = DecimalFormat("#.####")
+        df.roundingMode = RoundingMode.DOWN //無條件捨去
+        val roundedResult = df.format(result)
+
+        val message1 = "${typedValue.value} ${selectedConversion.value!!.convertFrom} is equal to"
+        val message2 = "$roundedResult ${selectedConversion.value!!.convertTo}"
+        ResultBlock(message1 = message1, message2 = message2)
     }
 }
